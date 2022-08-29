f98b35c48fbfe069f92f6a785551fcc49f095dce
Bruno <bruno@unimake.com.br>
2022-08-29 14:00:00 -0300

    CommitInicialLanchesMac

---
+++ LanchesMac/Migrations/20220829164435_AjusteTabelaLanche.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    public partial class AjusteTabelaLanche : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImagemThumbnaiUrl",
+                table: "Lanches",
+                newName: "ImagemThumbnailUrl");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImagemThumbnailUrl",
+                table: "Lanches",
+                newName: "ImagemThumbnaiUrl");
+        }
+    }
+}
